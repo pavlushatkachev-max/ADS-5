821e15d0d2eaa47444fc8a85b2fe49f597b998b7
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-04-18 23:39:06 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,33 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
+#include <iostream>
+template<typename T, int kSize>
 class TStack {
-  // добавьте код стека
+ private:
+  T arr[kSize];
+  int top;
+ public:
+  TStack() : top(-1) {}
+  int getSize() const { return top+1; }
+  bool isEmpty() const { return top == -1; }
+  void push(const T& item) {
+    if (top < kSize - 1)
+      arr[++top] = item;
+    else
+      throw "Full stack";
+  }
+  T get() const {
+    if (top >= 0)
+      return arr[top];
+    else
+      throw "Empty stack";
+  }
+  void pop() {
+    if (top >= 0)
+      top--;
+    else
+      throw "Empty stack";
+  }
 };
-
 #endif  // INCLUDE_TSTACK_H_
